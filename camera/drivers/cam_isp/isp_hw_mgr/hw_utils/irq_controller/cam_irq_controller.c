8705f7391faf7851768b75c3dbee76b798788ed4
Chandan Kumar Jha <cjha@qti.qualcomm.com>
2026-02-04 19:26:25 +0530

    msm: camera: isp: Enable per port start stop feature
    This change enables on the fly stop and start functionality,
    allowing the camera to be stopped and restarted dynamically
    using the same application without closing the test app.
    
    The customer is validating this using the per-port GST
    application, where they stop a camera and then restart
    the same camera that was previously closed—without relaunching
    the GST app. Therefore, no new IOCTL is required for this
    use case. We will continue using the same sensor group
    configuration that was received at the start of the use case.
    
    The per-port GST is a GUI-based tool that provides the ability
    to start and stop individual cameras. When the application is
    launched, both cameras (assuming a dual-camera setup)
    start by default. If the second camera is stopped, the
    application allows it to be started again dynamically.
    
    CRs-Fixed: 4351072
    Change-Id: Ib4c8bc2f4aa89d84f7d3ca67187e8a96675a343f
    Signed-off-by: Chandan Kumar Jha <cjha@qti.qualcomm.com>
    Signed-off-by: Ignatius Michael Jihan <mignatiu@qti.qualcomm.com>

--- camera/drivers/cam_isp/isp_hw_mgr/hw_utils/irq_controller/cam_irq_controller.c
+++ camera/drivers/cam_isp/isp_hw_mgr/hw_utils/irq_controller/cam_irq_controller.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/slab.h>
@@ -1084,7 +1084,8 @@ irqreturn_t cam_irq_controller_handle_irq(int irq_num, void *priv, int evt_grp)
 }
 
 int cam_irq_controller_update_irq(void *irq_controller, uint32_t handle,
-	bool enable, uint32_t *irq_mask)
+	bool enable, uint32_t *irq_mask,
+	void *bottom_half)
 {
 	struct cam_irq_controller   *controller  = irq_controller;
 	struct cam_irq_evt_handler  *evt_handler = NULL;
@@ -1101,6 +1102,9 @@ int cam_irq_controller_update_irq(void *irq_controller, uint32_t handle,
 		&evt_handler);
 	if (rc)
 		goto end;
+
+	if (bottom_half != NULL)
+		evt_handler->bottom_half = bottom_half;
 
 	__cam_irq_controller_disable_irq(controller, evt_handler);
 	for (i = 0; i < controller->num_registers; i++) {
